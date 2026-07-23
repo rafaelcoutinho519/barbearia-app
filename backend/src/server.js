2046a01d9e2d9a4503d635cc17d56a487a7311c8
rafaelcoutinho519 <rafaelcoutinho519@gmail.com>
2026-07-23 19:26:56 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,6 +32,6 @@ app.get('*', (req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor da barbearia rodando em http://localhost:${PORT}`);
 });
